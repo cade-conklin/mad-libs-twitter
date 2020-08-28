7f2d500611c6e82ae3f0545b2cef9d9d0ef28368
cade-conklin <conklincade@gmail.com>
2020-08-27 19:01:43 -0700

    added these files

--- bot.py
+++ bot.py
@@ -30,6 +30,15 @@ def get_random_noun(stuff, pos):
         counter += 1
 
 
+def get_random_adj(stuff, pos):
+    random_number = randrange(len(stuff[pos[1]]))
+    counter = 0
+    for i in range(0, len(stuff[pos[1]])):
+        if counter == random_number:
+            return stuff[pos[1]][i]
+        counter += 1
+
+
 def main():
     auth = OAuthHandler(os.environ.get('API_KEY'), os.environ.get('API_SECRET_KEY'))
     auth.set_access_token(os.environ.get('ACCESS_TOKEN'),
@@ -44,6 +53,11 @@ def main():
 
     with open('nouns-tagged.json', 'r+') as out_json:
         noun_data = json.load(out_json)
+        # for word in data:
+        #     print(data[word][0][0][1])
+
+    with open('adjectives-tagged.json', 'r+') as out_json:
+        adj_data = json.load(out_json)
         # for word in data:
         #     print(data[word][0][0][1])
 
@@ -68,8 +82,8 @@ def main():
                     break
                 if len(pos[0]) == 1:
                     continue
-                rand_num = randint(1, 5)
-                if True:
+                rand_num = randint(1, 2)
+                if rand_num == 1:
                     if pos[1] == "VB" or pos[1] == "VBG" or pos[1] == "VBN":
                         random_verb = get_random_verb(verb_data, pos)
                         full_tweet = full_tweet.replace(str(pos[0]), str(random_verb), 1)
@@ -78,9 +92,17 @@ def main():
                         random_noun = get_random_noun(noun_data, pos)
                         full_tweet = full_tweet.replace(str(pos[0]), str(random_noun), 1)
                         continue
+                    if pos[1] == "JJ" or pos[1] == "JJS":
+                        random_adj = get_random_adj(adj_data, pos)
+                        full_tweet = full_tweet.replace(str(pos[0]), str(random_adj), 1)
+                        continue
                 new_tweet += pos[0] + " "
-            # api.update_status(new_tweet)
-            output_text.write("******" + full_tweet + "\n\n")
+            try:
+                if len(full_tweet) < 280:
+                    api.update_status(full_tweet)
+                    output_text.write("******" + full_tweet + "\n\n")
+            except:
+                print("error updating status")
             # output_text.write(new_tweet + "\n\n\n\n")
 
 
